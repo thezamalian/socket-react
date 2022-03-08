047a8994e6fb6584a068d8fa7fe37bc69f473541
Zamal Hossain <thezamalian@gmail.com>
2022-03-08 23:22:20 +0600

    basic socket server setup

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const expressServer = http.createServer(app);
+const path = require('path');
+const { Server } = require('socket.io');
+const io = new Server(expressServer);
+
+const port = 5000 | process.env.PORT;
+
+app.use(express.static("client/build"))
+
+app.get('*', function (req, res) {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
+app.get('/express-server', function (req, res) {
+    res.end("This is my express server");
+});
+
+io.on('connection', function () {
+    console.log("New User Connected");
+
+    socket.on('disconnect', function () {
+        console.log("User Disconnected");
+    });
+});
+
+expressServer.listen(port, function () {
+    console.log("Server listening on port " + port);
+});
