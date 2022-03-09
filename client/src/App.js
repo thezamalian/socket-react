c032bd06d110cf9c42b0ebfa762ac7984c62063c
Zamal Hossain <thezamalian@gmail.com>
2022-03-09 11:45:11 +0600

    socket connection between frontend and backend is established

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import io from 'socket.io-client'
 
 const App = () => {
+  const [msg, setMsg] = useState("");
+
+  // useEffect(() => {
   const socket = io.connect('/');
+  socket.on('msg', (data) => setMsg(data));
+  // })
 
   return (
     <div>
-
+      <h1>Hello, React App with Socket</h1>
+      <h2>{msg}</h2>
     </div>
   );
 };
--- server.js
+++ server.js
@@ -18,8 +18,12 @@ app.get('/express-server', function (req, res) {
     res.end("This is my express server");
 });
 
-io.on('connection', function () {
+io.on('connection', function (socket) {
     console.log("New User Connected");
+
+    setTimeout(() => {
+        socket.emit('msg', "This is a message from socket server.");
+    }, 5000);
 
     socket.on('disconnect', function () {
         console.log("User Disconnected");
